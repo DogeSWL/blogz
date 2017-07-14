edf2d57881097487f613298bd1e074805ff3e771
DogeSWL <lusen.w1387@gmail.com>
2017-07-13 22:54:23 -0700

    seperated models and app

--- main.py
+++ main.py
@@ -1,34 +1,6 @@
-from flask import Flask, request, redirect, flash, render_template, session
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:MyNewPass@localhost:8889/blogz'
-app.config['SQLALCHEMY_ECHO'] = True
-
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
-    blogs = db.relationship('Blog', backref='owner')
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-
-class Blog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.String(120))
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __init__(self, title, body, owner_id):
-        self.title = title
-        self.body = body
-        self.owner_id = owner_id
+from flask import request, redirect, flash, render_template, session
+from models import User, Blog
+from app import app, db
 
 def get_blogList():
     return Blog.query.all()
