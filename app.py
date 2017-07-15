cbe1d88f9f59a981bbd5823e57dc1b32da2910cd
DogeSWL <lusen.w1387@gmail.com>
2017-07-15 15:20:34 -0700

    created config file and moved key and various config info into it

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:MyNewPass@localhost:8889/blogz'
-app.config['SQLALCHEMY_ECHO'] = True
-
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RU'
+app.config.from_object('config')
 
 db = SQLAlchemy(app)
---
+++ config.py
@@ -0,0 +1,6 @@
+DEBUG = True
+
+SQLALCHEMY_DATABASE_URI= 'mysql+pymysql://blogz:MyNewPass@localhost:8889/blogz'
+SQLALCHEMY_ECHO = True
+
+secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RU'
