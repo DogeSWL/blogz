7c5ab996f6fb1f355df6f343eac923f7b94224c3
DogeSWL <lusen.w1387@gmail.com>
2017-07-16 20:24:55 -0700

    removed addblog route from main and into view folder, reworked get_blogList func

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from flask import request, redirect, flash, render_template, session
 from models import User, Blog
 from app import app, db
+
 from views.login import login
 from views.signup import signup
+from views.addBlog import addBlog
 
 def get_blogList():
-    return Blog.query.all()
+    return db.engine.execute('''SELECT user.username AS username, blog.title as title, blog.body as body
+                                FROM blog
+                                LEFT JOIN user ON user.id = blog.owner_id''')
 
 def checkSession():
     if session:
         return 'True'
     else:
         return 'False'
-
 
 @app.before_request
 def require_login():
@@ -25,33 +28,6 @@ def logout():
     del session['username']
     return redirect('/blog')
 
-@app.route('/addBlog', methods=['POST'])
-def add_Blog():
-    new_blogTitle = request.form['blog_Title']
-    new_blogEntry = request.form['blog_NewEntry']
-
-    # grab current user
-    owner = User.query.filter_by(username=session['username']).first()
-
-    title_Error = ''
-    entry_Error = ''
-
-    if new_blogTitle == '':
-        title_Error = 'Title is empty'
-    if new_blogEntry == '':
-        entry_Error = 'Entry is empty'
-
-    if (title_Error != '') or (entry_Error != ''):
-        return render_template('newpost.html',
-                                title_Error = title_Error,
-                                entry_Error = entry_Error)
-    else:
-        blog = Blog(title=new_blogTitle, body=new_blogEntry, owner_id=owner.id)
-        db.session.add(blog)
-        db.session.commit()
-
-        return redirect('/blog?id='+str(blog.id))
-
 @app.route("/newpost")
 def newpost_page():
     return render_template('newpost.html',
@@ -59,19 +35,21 @@ def newpost_page():
 
 @app.route('/blog', methods=['GET'])
 def blog():
-
     some_id = request.args.get('id') # extract the value of id
     userID = request.args.get('user')
 
+    # blogData = Blog.query.filter_by(owner_id=userID)
+    # userData = User.query.filter_by(id=userID).first()
+
     if userID:
-        userPage_blogs = Blog.query.filter_by(owner_id=userID)
-        userPage_username = User.query.filter_by(id=userID).first()
         return render_template('singleUser.html',
-                                userPage_blogs=userPage_blogs,
-                                userPage_username=userPage_username,
+                                blogData=blogData,
+                                userData=userData,
                                 sessionCheck=checkSession())
 
     if some_id == None: # if value of id returns None render template blog.html
+
+
         return render_template('blog.html',
                                 blogList=get_blogList(),
                                 sessionCheck=checkSession())
@@ -84,7 +62,6 @@ def blog():
 @app.route('/index')
 def home():
     all_users = User.query.all()
-    # sessionCheck = checkSession()
 
     return render_template('index.html',
                             all_users=all_users,
